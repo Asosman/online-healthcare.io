138b93997adaab90bfcac3e3afb47a7b9fcdb689
asosman <uchechukwu.asogwa.247516@unn.edu.ng>
2025-05-26 07:48:55 +0100

    update navbar.js

--- navbar.js
+++ navbar.js
@@ -50,28 +50,46 @@ function navbar() {
     <div id="inputnav1">
         <div id="inputnavright1">
             <div>
-                <select id="city" name="city">
-                    <option value="New Delhi"> New Delhi</option>
-                    <option value="Alipur">Alipur</option>
-                    <option value="Bawana">Bawana</option>
-                    <option value="Central Delhi">Central Delhi</option>
-                    <option value="Delhi">Delhi</option>
-                    <option value="Deoli">Deoli</option>
-                    <option value="East Delhi">East Delhi</option>
-                    <option value="Karol Bagh">Karol Bagh</option>
-                    <option value="Najafgarh">Najafgarh</option>
-                    <option value="Nangloi Jat">Nangloi Jat</option>
-                    <option value="Narela">Narela</option>
-                    <option value="New Delhi">New Delhi</option>
-                    <option value="North Delhi">North Delhi</option>
-                    <option value="North East Delhi">North East Delhi</option>
-                    <option value="North West Delhi">North West Delhi</option>
-                    <option value="Pitampura">Pitampura</option>
-                    <option value="Rohini">Rohini</option>
-                    <option value="South Delhi">South Delhi</option>
-                    <option value="South West Delhi">South West Delhi</option>
-                    <option value="West Delhi">West Delhi</option>
-                </select>
+               <select name="city" id="city">
+  <option value="">--Select State--</option>
+  <option value="Abia">Abia</option>
+  <option value="Adamawa">Adamawa</option>
+  <option value="Akwa Ibom">Akwa Ibom</option>
+  <option value="Anambra">Anambra</option>
+  <option value="Bauchi">Bauchi</option>
+  <option value="Bayelsa">Bayelsa</option>
+  <option value="Benue">Benue</option>
+  <option value="Borno">Borno</option>
+  <option value="Cross River">Cross River</option>
+  <option value="Delta">Delta</option>
+  <option value="Ebonyi">Ebonyi</option>
+  <option value="Edo">Edo</option>
+  <option value="Ekiti">Ekiti</option>
+  <option value="Enugu">Enugu</option>
+  <option value="FCT">FCT</option>
+  <option value="Gombe">Gombe</option>
+  <option value="Imo">Imo</option>
+  <option value="Jigawa">Jigawa</option>
+  <option value="Kaduna">Kaduna</option>
+  <option value="Kano">Kano</option>
+  <option value="Katsina">Katsina</option>
+  <option value="Kebbi">Kebbi</option>
+  <option value="Kogi">Kogi</option>
+  <option value="Kwara">Kwara</option>
+  <option value="Lagos">Lagos</option>
+  <option value="Nasarawa">Nasarawa</option>
+  <option value="Niger">Niger</option>
+  <option value="Ogun">Ogun</option>
+  <option value="Ondo">Ondo</option>
+  <option value="Osun">Osun</option>
+  <option value="Oyo">Oyo</option>
+  <option value="Plateau">Plateau</option>
+  <option value="Rivers">Rivers</option>
+  <option value="Sokoto">Sokoto</option>
+  <option value="Taraba">Taraba</option>
+  <option value="Yobe">Yobe</option>
+  <option value="Zamfara">Zamfara</option>
+</select>
             </div>
             <div>
                
